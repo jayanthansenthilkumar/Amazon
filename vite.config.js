0c8dd63cf07cd5f63f23b83e0643efd2a0753694
jayanthansenthilkumar <jayanthansenthilkumar18@gmail.com>
2025-07-29 12:42:27 +0530

    Cart created

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port:65535, // change this to your desired port (e.g., last used port)
+  },
+});
